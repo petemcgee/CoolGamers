94ef9e81a462a2238c99fea07c68e97886039a96
petemcgee <38958234+petemcgee@users.noreply.github.com>
2019-03-30 22:27:24 -0600

    Updated These Files

--- shtop-lookinge.js
+++ shtop-lookinge.js
@@ -40,7 +40,7 @@ document.addEventListener('keydown', function(e) {
 function activateCheats() {
   document.body.style.backgroundImage = "url('files/leet-haxor.jpg')";
   document.body.style.backgroundRepeat = "no-repeat"
-  document.body.style.backgroundSize = "auto"
+  document.body.style.backgroundSize = "50%"
 
   var audio = new Audio('audio/HACKERMAN.mp3');
   audio.play();
